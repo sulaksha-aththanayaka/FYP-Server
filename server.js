8777b5c113baef96fc1d2d383de3010212c87f7f
sulaksha-aththanayaka <sulakshaa04@gmail.com>
2024-06-08 20:05:59 +0530

    Python script added - video play when SearchBar component mounted

--- server.js
+++ server.js
@@ -29,6 +29,47 @@ app.use("/api/videos", videoRoutes);
 app.use("/api/sign-upload", signUploadRoutes);
 app.use("/api/fetch-videos", fetchVideosRoutes);
 
+// New endpoint to run the Python script
+app.post("/api/run-python", (req, res) => {
+    // const { image } = req.body;
+    // const image = 'girl1.jpg'
+    // const pythonProcess = spawn('python', ['detect.py', '--image', image]);
+    const pythonProcess = spawn('python', ['detect.py']);
+
+    let dataToSend = "";
+    pythonProcess.stdout.on('data', (data) => {
+        dataToSend += data.toString();
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    // pythonProcess.on('close', (code) => {
+    //     console.log(`child process exited with code ${code}`);
+    //     res.json({ result: dataToSend });
+    // });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
+        
+        // Assuming the Python script outputs lines like "Gender: Male" and "Age: 25-32 years"
+        const outputLines = dataToSend.split('\n');
+        let gender = '';
+        let age = '';
+
+        outputLines.forEach(line => {
+            if (line.startsWith('Gender:')) {
+                gender = line.split('Gender: ')[1].trim();
+            } else if (line.startsWith('Age:')) {
+                age = line.split('Age: ')[1].trim();
+            }
+        });
+
+        res.json({ gender, age });
+    });
+});
+
 
 
 app.use(errorHandler);
